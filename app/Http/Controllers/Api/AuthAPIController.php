c7e615358c6ae54a1ebc122264a877561be47fc6
mohsinahmad <mohsinahamd@axact.com>
2018-10-25 13:29:37 +0500

    user detail update

--- app/Http/Controllers/Api/AuthAPIController.php
+++ app/Http/Controllers/Api/AuthAPIController.php
@@ -868,11 +868,11 @@ class AuthAPIController extends AppBaseController
         }
 
         if (count($details) > 0) {
-//            if (!empty($details['phone'])) {
-//                $details['phone'] = $details['country_code'] . $details['phone'];
-//                unset($details['country_code']);
-//            }
-            //$this->userDetailRepository->update($details, $user->details->id);
+            if (!empty($details['phone'])) {
+                $details['phone'] = $details['country_code'] . $details['phone'];
+                unset($details['country_code']);
+            }
+            $this->userDetailRepository->update($details, $user->details->id);
         }
 
         return $this->sendResponse(['user' => $this->userRepository->findWithoutFail($user->id)->toArray()], 'Profile Updated Successfully');
