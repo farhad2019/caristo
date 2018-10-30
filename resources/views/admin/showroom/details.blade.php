c612233b4a93836535e8864c829bd0616981c7e1
mohsinahmad <mohsinahamd@axact.com>
2018-10-30 21:08:35 +0500

    hide showroom panel assets

--- resources/views/admin/showroom/details.blade.php
+++ resources/views/admin/showroom/details.blade.php
@@ -136,7 +136,7 @@
                 <h4>Place a Bid now</h4>
                 {!! Form::open(['route' => 'admin.makeBids.store']) !!}
                 <input type="text" name="" placeholder="AED"
-                       {{ isset($bid)? 'readonly':'required' }} value="{{ isset($bid)? number_format($bid->amount):0 }}">
+                       {{ isset($bid)? 'readonly':'required' }} value="{{ isset($bid)? number_format($bid->amount):'' }}">
                 <button type="submit" class="submit" name="">submit</button>
                 {!! Form::hidden('car_id', $car->id) !!}
                 {!! Form::close() !!}
