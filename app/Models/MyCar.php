f9b68edec9bd491fab809a937ac73efbad64f859
farhadali <farhad.surani@gmail.com>
2018-12-18 11:59:48 +0500

    #Car with mulitple region

--- app/Models/MyCar.php
+++ app/Models/MyCar.php
@@ -481,6 +481,11 @@ class MyCar extends Model
         return $this->hasMany(CarInteraction::class, 'car_id')->where('type', CarInteraction::TYPE_FAVORITE);
     }
 
+    public function userInteractions()
+    {
+        return $this->hasMany(CarInteraction::class, 'car_id');
+    }
+
     public function carRegions()
     {
         return $this->hasMany(CarRegion::class, 'car_id');
