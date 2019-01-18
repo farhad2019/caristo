8ff305efa3d7798f1931fe4257bbd5b55c5325c2
Shakir Mirza <shakirmirza@axact.com>
2019-01-18 16:36:29 +0500

    QA fixes cycle 1

--- app/Http/Requests/Api/UpdateForgotPasswordRequest.php
+++ app/Http/Requests/Api/UpdateForgotPasswordRequest.php
@@ -4,14 +4,24 @@ namespace App\Http\Requests\Api;
 
 use App\Http\Requests\BaseAPIRequest;
 
+/**
+ * @property mixed verification_code
+ * @property mixed password
+ * @property mixed email
+ */
 class UpdateForgotPasswordRequest extends BaseAPIRequest
 {
-
+    /**
+     * @return bool
+     */
     public function authorize()
     {
         return true;
     }
 
+    /**
+     * @return array
+     */
     public function rules()
     {
         return [
@@ -21,5 +31,4 @@ class UpdateForgotPasswordRequest extends BaseAPIRequest
             'password_confirmation' => 'min:6'
         ];
     }
-
 }
